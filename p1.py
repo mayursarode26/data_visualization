4ca07274f573cea6c8e600c86fb1d890f5a21571
mayur <mayursarode985@gmail.com>
2026-04-23 17:21:46 +0530

    version-2

--- p1.py
+++ p1.py
@@ -3,3 +3,10 @@ if a == a[::-1]:
 	print("String is a palindrome")
 else:
 	print("Not a palindrome")
+
+b= 121
+num_str = str(b)
+if num_str == num_str[::-1]:
+	print("Number is a palindrome")
+else:
+	print("Number is not a palindrome")
